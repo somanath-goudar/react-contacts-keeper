06e76535ae9e7a69135e91c1a089e904a1be8a5c
Somanath Goudar <somugowda8050@gmail.com>
2021-06-29 15:55:26 +0530

    Frontend CRUD Complete without DB

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContactForm from '../contacts/ContactForm';
+import Contacts from '../contacts/Contacts';
+import ContactFilter from '../contacts/ContactFilter';
+
+const Home = () => {
+  return (
+    <div className='grid-2'>
+      <div>
+        <ContactForm />
+      </div>
+
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
